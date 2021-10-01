085b3c7b60948db30536e69edac0ec62ba52aeaa
unknown <alfahrimudzik@gmail.com>
2021-10-01 14:28:57 +0700

    centering text

--- components/layout.tsx
+++ components/layout.tsx
@@ -11,7 +11,7 @@ const Layout = ({ children }: any): JSX.Element => {
       <div className="min-h-screen bg-cusblue">
         {/* body */}
         <Navbar />
-        <div className="max-w-3xl mx-auto pt-40 md:pt-32 text-gray-800 ">
+        <div className="max-w-3xl mx-auto pt-40 md:pt-28 text-gray-800 ">
           {children}
         </div>
         <Footer />
--- pages/index.tsx
+++ pages/index.tsx
@@ -7,14 +7,14 @@ const Home: NextPage = () => {
   return (
     <Layout>
       <div className="flex justify-between place-items-center">
-        <div className="mr-12">
-          <div className="w-32 h-32 bg-gradient-to-r from-pink-200 to-purple-400 rounded-full"></div>
-        </div>
-        <div className="">
-          <h3 className="text-3xl pb-2 border-b border-gray-300 w-max font-bold mb-3 flex items-center">
+        <div className="text-center">
+          <div className="mb-10">
+            <div className="w-full h-32 mx-auto bg-gradient-to-r from-pink-200 to-purple-400 rounded-full"></div>
+          </div>
+          <h3 className="text-3xl pb-2 mx-auto border-b border-gray-300 w-max font-bold mb-3 flex items-center">
             <span className="mr-1">Hey</span> 👋
           </h3>
-          <p className="text-gray-800 text-xl leading-loose">
+          <p className="text-gray-800 text-xl leading-loose w-11/12 mx-auto">
             My name is <span className="font-bold">Fahri</span> and I{"'"}m from
             Yogyakarta, Indonesia. I’m a third year computer engineering student
             at Diponegoro University. I love javascript and react. I{"'"}d like
@@ -23,7 +23,7 @@ const Home: NextPage = () => {
         </div>
       </div>
 
-      <div className="my-10 flex place-items-center hover:shadow-lg duration-200 text-indigo-400 justify-center w-1/2 mx-auto px-5 py-5 bg-white border border-gray-200 rounded-xl">
+      <div className="my-10 flex place-items-center hover:shadow-lg duration-200 text-purple-500 justify-center w-1/2 mx-auto px-5 py-5 bg-white border border-gray-200 rounded-xl">
         <a
           href="https://github.com/mudzikalfahri"
           target="_blank"
@@ -50,7 +50,7 @@ const Home: NextPage = () => {
         </a>
       </div>
 
-      <div className="mt-20 mb-10">
+      <div className="mt-20 mb-20">
         <h1 className="text-4xl font-bold mb-6 leading-snug pb-2 border-b border-gray-300 w-max">
           A few project
           <br /> that i{"'"}ve been built
