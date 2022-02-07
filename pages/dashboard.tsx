de5c60b7f111a70719e2a1def3f50b20ac9bd5b0
mudzikalfahri <alfahrimudzik@gmail.com>
2022-02-07 19:20:20 +0700

    feat: add button component

---
+++ pages/dashboard.tsx
@@ -0,0 +1,7 @@
+import { NextPage } from "next";
+
+const Dashboard: NextPage = () => {
+  return <div className=""></div>;
+};
+
+export default Dashboard;
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,7 @@ import ProjectCard from "@components/projectcard";
 import Layout from "@components/layout";
 import { data } from "@utils/data";
 import { motion } from "framer-motion";
+import Button from "@components/button";
 
 const Home: NextPage = () => {
   return (
@@ -64,19 +65,7 @@ const Home: NextPage = () => {
               computer engineering at Diponegoro University.`}
             </p>
           </motion.div>
-          <motion.div
-            initial={{ opacity: 0, y: 50 }}
-            animate={{ opacity: 1, y: 0 }}
-            transition={{
-              duration: 0.5,
-              delay: 0.6,
-              type: "spring",
-            }}
-            className="py-2 mt-6 dark:bg-gray-700/60 cursor-pointer px-4 max-w-max font-medium rounded-md bg-gray-700 border border-gray-500 relative group overflow-hidden text-sm text-center text-white"
-          >
-            <div className="h-0.5 group group-hover:w-full rounded-lg duration-200 w-0 bg-white absolute left-0 bottom-0"></div>
-            See my Works
-          </motion.div>
+          <Button name="See my Works" />
         </div>
       </motion.div>
 
