8eb0aa2d322ba920ec1552991e94df8bf05b8d9e
mudzikalfahri <alfahrimudzik@gmail.com>
2022-06-06 08:16:03 +0700

    update link

--- utils/data.ts
+++ utils/data.ts
@@ -9,12 +9,12 @@ export const data = [
     title: "Medio Blog",
     stack: ["nextjs", "graphql", "tailwind", "prisma"],
     desc: "Blogging website that helps people share their own article to the community",
-    link: "https://github.com/mudzikalfahri/corner-football",
+    link: "https://github.com/mudzikalfahri/medio.git",
   },
   {
     title: "Wecoding",
     stack: ["nextjs", "typescript", "tailwind", "express", "mongodb"],
     desc: "Fullstack coding forum website built with nextjs, tailwind, express, and authentication using jwt",
-    link: "https://github.com/mudzikalfahri/wecoding",
+    link: "https://wecoding.vercel.app",
   },
 ];
